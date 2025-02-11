3f9b86fbf13e61905522f06da7101634349c5628
mvasquezb99 <mvasquezb@eafit.edu.co>
2025-02-11 13:15:33 -0500

    feat: tutorial-1

---
+++ tutorial-1/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
+
+Route::get('/about', function () {
+
+    $data1 = "About us - Online Store";
+
+    $data2 = "About us";
+
+    $description = "This is an about page ...";
+
+    $author = "Developed by: Your Name";
+
+    return view('home.about',[
+        "title" => $data1,
+        "subtitle" => $data2,
+        "description" => $description,
+        "author" => $author,
+    ]);
+})->name("home.about");
+
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
+
+Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name("product.create");
+
+Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name("product.save");
+
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
+
+
