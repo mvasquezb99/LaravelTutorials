059fe3babe763e25c0456a50489bdb105e68134c
mvasquezb99 <mvasquezb@eafit.edu.co>
2025-03-23 09:22:32 -0500

    feat: Tutorial services implemented

---
+++ tutorial-1/app/Http/Controllers/Api/ProductApiController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class ProductApiController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $products = Product::all();
+        return response()->json($products, 200);
+    }
+
+    public function show(string $id): JsonResponse
+    {
+        $product = Product::findOrFail($id);
+        return response()->json($product, 200);
+    }
+
+    public function create(Request $request): JsonResponse
+    {
+        Product::create($request->only(['name', 'price']));
+        return response()->json('Successfully created', 200);
+    }
+}
